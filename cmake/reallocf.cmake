db45652646514b8a7d8b48e4a53d85b49514676c
Collin Funk <collin.funk1@gmail.com>
2023-10-05 09:45:50 -0700

    Added FreeBSD-like reallocf ().

---
+++ cmake/reallocf.cmake
@@ -0,0 +1,23 @@
+
+include_guard(GLOBAL)
+
+include($CACHE{LIBCFUNK_MODULE_DIR}/stdlib-h.cmake)
+include($CACHE{LIBCFUNK_MODULE_DIR}/realloc.cmake)
+
+if (HAVE_STDLIB_H)
+  check_symbol_exists("reallocf" "stdlib.h" HAVE_REALLOCF)
+else ()
+  set(HAVE_REALLOCF "" CACHE INTERNAL "")
+endif ()
+
+set(LIBCFUNK_DECLARE_REALLOCF "1" CACHE INTERNAL "")
+
+if (NOT HAVE_REALLOCF)
+  target_sources("$CACHE{LIBCFUNK_LIBRARY_NAME}" PRIVATE
+    $CACHE{LIBCFUNK_SOURCE_DIR}/reallocf.c
+  )
+endif ()
+
+if (LIBCFUNK_ENABLE_TESTS)
+  include($CACHE{LIBCFUNK_MODULE_DIR}/test-reallocf.cmake)
+endif ()
---
+++ cmake/test-reallocf.cmake
@@ -0,0 +1,16 @@
+
+include_guard(GLOBAL)
+
+add_executable(test-reallocf)
+
+target_link_libraries(test-reallocf PRIVATE
+  "$CACHE{LIBCFUNK_LIBRARY_NAME}"
+)
+
+target_sources(test-reallocf PRIVATE
+  $CACHE{LIBCFUNK_TEST_SOURCE_DIR}/test-reallocf.c
+)
+
+set_target_properties(test-reallocf PROPERTIES
+  RUNTIME_OUTPUT_DIRECTORY $CACHE{LIBCFUNK_TEST_BUILD_DIR}
+)
