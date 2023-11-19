87378ed8c52e8c1f04b9a7a8c58d488d6f294810
Collin Funk <collin.funk1@gmail.com>
2023-11-19 04:21:29 -0800

    Add prototypes for formatted I/O functions that need implementations

---
+++ cmake/snprintf.cmake
@@ -0,0 +1,13 @@
+
+include_guard(GLOBAL)
+
+include($CACHE{LIBCFUNK_MODULE_DIR}/stdio-h.cmake)
+
+if (HAVE_STDIO_H)
+  check_symbol_exists("snprintf" "stdio.h" HAVE_SNPRINTF)
+endif ()
+
+if (NOT HAVE_SNPRINTF)
+  message(FATAL_ERROR "Your system does not have an implementation for 'snprintf'.")
+endif ()
+
